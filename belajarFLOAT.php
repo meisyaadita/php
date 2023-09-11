b012b5e222e5cf259f5ef5ac707d3790d0cd548b
meisyaadita <144640450+meisyaadita@users.noreply.github.com>
2023-09-11 09:36:28 +0700

    Add files via upload

---
+++ belajarFLOAT.php
@@ -0,0 +1,13 @@
+<?php
+	echo "___TIPE DATA FLOAT";
+	echo "<br>";
+	$a = 5.234;
+	$b = 3.2e4;
+	$c = 4E-10;
+	var_dump($a)
+	echo "<br>";
+	var_dump($b)
+	echo "<br>";
+	var_dump($c)
+	echo "<br><br>";
+?>
---
+++ belajarINTEGER.php
@@ -0,0 +1,13 @@
+<?php
+	echo "___TIPE DATA INTEGER";
+	echo "<br /";
+	$x = 1945;
+	var_dump($x);
+	echo "<br>$x<br>";
+	$y = 0127
+	$z = 0x1B
+	var_dump($y)
+	echo "<br>";
+	var_dump($z)
+	echo "<br><br>";
+?>
